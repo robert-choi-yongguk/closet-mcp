94a94eb59275358e9c3fa253735084f585f29e06
Robert Choi <robert.choi@clo3d.com>
2025-05-14 08:34:32 +0900

    Update project configuration to include MCP tool metadata and additional package references

--- Closet.MCP.Server/Program.cs
+++ Closet.MCP.Server/Program.cs
@@ -5,9 +5,6 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
+builder.EnableMcpToolMetadata();
 
 builder.Build().Run();
